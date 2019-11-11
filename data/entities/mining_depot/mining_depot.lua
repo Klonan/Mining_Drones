3b29e0a6f13d83105c591b7722b9a2dc75ada574
Klonan <11986037+Klonan@users.noreply.github.com>
2019-11-11 21:56:11 +0100

    first integration of depot graphics

--- data/entities/mining_depot/mining_depot.lua
+++ data/entities/mining_depot/mining_depot.lua
@@ -5,11 +5,12 @@ machine.localised_name = {name}
 local scale = 2
 util.recursive_hack_make_hr(machine)
 util.recursive_hack_scale(machine, scale)
-machine.collision_box = {{-2.7, -2.7},{2.7, 2.7}}
-machine.selection_box = {{-2.9, -2.9},{2.9, 2.9}}
+machine.collision_box = {{-1.5, -2.5},{1.5, 2.5}}
+machine.selection_box = {{-1.5, -2.5},{1.5, 2.5}}
 machine.crafting_categories = {name}
 machine.crafting_speed = (1)
 machine.ingredient_count = nil
+--machine.collision_mask = {"item-layer", "object-layer", "water-tile"}
 machine.allowed_effects = {"consumption", "speed", "pollution"}
 machine.module_specification =
 {
@@ -25,7 +26,7 @@ machine.fluid_boxes =
     pipe_covers = nil,
     base_area = 1,
     base_level = 1,
-    pipe_connections = {{ type="output", position = {0, -3} }},
+    pipe_connections = {{ type="output", position = {0, -2.6} }},
   },
   off_when_no_fluid_recipe = false
 }
@@ -38,6 +39,78 @@ machine.energy_source =
   emissions_per_second_per_watt = 1 / 180000
 }
 machine.is_deployer = true
+
+local base = function(shift)
+  return
+  {
+    filename = util.path("data/entities/mining_depot/depot-base.png"),
+    width = 474,
+    height = 335,
+    frame_count = 1,
+    scale = 0.5,
+    shift = shift
+  }
+end
+
+local h_chest = function(shift)
+  return
+  {
+    filename = util.path("data/entities/mining_depot/depot-chest-h.png"),
+    width = 190,
+    height = 126,
+    frame_count = 1,
+    scale = 0.5,
+    shift = shift
+  }
+end
+
+local v_chest = function(shift)
+  return
+  {
+    filename = util.path("data/entities/mining_depot/depot-chest-v.png"),
+    width = 136,
+    height = 189,
+    frame_count = 1,
+    scale = 0.5,
+    shift = shift
+  }
+end
+
+machine.animation =
+{
+  north =
+  {
+    layers =
+    {
+      base({0, -1}),
+      h_chest({0, 1.5})
+    }
+  },
+  south =
+  {
+    layers =
+    {
+      h_chest({0, -1.5}),
+      base({0, 1}),
+    }
+  },
+  east =
+  {
+    layers =
+    {
+      v_chest({-1.5, 0}),
+      base({1, 0}),
+    }
+  },
+  west =
+  {
+    layers =
+    {
+      base({-1, 0}),
+      v_chest{1.5, 0},
+    }
+  },
+}
 
 local item = {
   type = "item",
