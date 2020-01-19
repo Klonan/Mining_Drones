b1e11ee6f3f9fd6e6282676a21d4b7bf47ee775b
Klonan <11986037+Klonan@users.noreply.github.com>
2020-01-19 18:13:41 +0100

    Fix base game pathfinder crashes

--- data/entities/mining_drone/mining_drone_entity.lua
+++ data/entities/mining_drone/mining_drone_entity.lua
@@ -9,6 +9,9 @@ local sound = data.raw.tile["grass-1"].walking_sound
 --local mining_drone_flags = {"placeable-off-grid", "hidden", "not-in-kill-statistics"}
 local mining_drone_flags = {"placeable-off-grid", "hidden"}
 
+--local mining_drone_collision_mask = {"not-colliding-with-itself", "player-layer", "train-layer", "consider-tile-transitions"}
+local mining_drone_collision_mask = {"not-colliding-with-itself", "player-layer", "train-layer"}
+
 local shuffle = function(n, v)
   --local n = n or 0.5
   --log("Shuffling: "..n..v)
@@ -63,7 +66,7 @@ local make_drone = function(name, tint)
     healing_per_tick = 0.1,
     --minable = {result = name, mining_time = 2},
     collision_box = {{-0.18, -0.18}, {0.18, 0.18}},
-    collision_mask = {"not-colliding-with-itself", "player-layer", "train-layer", "consider-tile-transitions"},
+    collision_mask = mining_drone_collision_mask,
     max_pursue_distance = 64,
     resistances = nil,
     min_persue_time = 60 * 15,
